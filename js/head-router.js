58eca59bfa6d3ee2b95c36943b9e82670e5b7684
liu bo <916321714@qq.com>
2015-06-09 13:28:51 +0800

    2

--- js/head-router.js
+++ js/head-router.js
@@ -22,36 +22,46 @@ PoiArray[2]="http://www.qq.com";
 PoiArray[3]="http://www.taobao.com";
 
 num=0;
-
-
-$(document).ready(function(){
-	//-调整全部属性
-
+blb='blb1';
+	var Refresh=function(){
 		$('.ban-list-router-img').attr('src',PicArray[num]);
 		$('.ban-list-router-txt center').text(TxtArray[num]);
 		$('.ban-list-a').attr('href',PoiArray[num]);
+		$('.ban-list-bar').attr('class','ban-list-bar ban-list-bar-sto');
+		$('#'+blb).attr('class','ban-list-bar ban-list-bar-act');
+	};
+	var OnClk=function(){
+		$('.ban-list-bar').click(function(){
+		var btn=$(this);
+		$('.ban-list-bar').attr('class','ban-list-bar ban-list-bar-sto');
+		btn.attr('class','ban-list-bar ban-list-bar-act');
 
+		var ChosId=btn.attr('id');
+		num=ChosId.substring(3);
+		num=num--;
 
-		$(".ban-list-bar").click(function(){
-		//-按钮点击事件
-			var btn=$(this);
-			$('.ban-list-bar').attr('class','ban-list-bar ban-list-bar-sto');
-			btn.attr('class','ban-list-bar ban-list-bar-act');
+		Refresh();
+		});
+	};
 
-			var ChosId=btn.attr('id');
-			num=ChosId.substring(3);
-			num=num-1;
+	var AutoChange=setInterval(function(){
 
-			$('.ban-list-router-img').attr('src',PicArray[num]);
-			$('.ban-list-router-txt center').text(TxtArray[num]);
-			$('.ban-list-a').attr('href',PoiArray[num]);
+		if(num<3){
+			num++;
 
+		}else{
+			num=0;
+		}
+		var blb=num+1;
+		blb="blb"+blb;
 
-		//按钮点击事件-
-	});
+		Refresh();
 
-
-
+	}, 3000);
+$(document).ready(function(){
+	Refresh();
+	OnClk();
+	AutoChange();
 
 
 });
